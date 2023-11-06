606a6e3c9c6fe5c9e8086249b290febf52d727bf
m_parrella <parrella.martin@gmail.com>
2023-11-06 16:31:03 -0300

    Se crea un proyecto para simular un Servicio externo con compatibilidad para Docker exponiendo una API para obtener la fecha. Se modifica el Layout del proyecto principal para consultar la API del servicio externo y mostrar la fecha en el Footer.

---
+++ ExternalService/Controllers/APIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExternalService.Controllers
+{
+    public class APIController : ApiController
+    {
+        [Route("date/")]
+        [HttpGet]
+        public IHttpActionResult GetDate()
+        {
+            return Ok(DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+        }
+       
+    }
+}
